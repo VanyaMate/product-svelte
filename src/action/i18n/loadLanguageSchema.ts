4414b3bd958298d14f6f1430120bd31b644b8c51
VanyaMate <buhu653@mail.ru>
2024-06-25 13:52:59 +0300

    remove i18n

--- src/action/i18n/loadLanguageSchema.ts
+++
@@ -1,7 +0,0 @@
-export const loadLanguageSchema = function (language: string) {
-    return fetch(`/locale/${ language }.json`, {
-        method : 'GET',
-        headers: { 'Content-Type': 'application/json' },
-    })
-        .then((response) => response.json());
-};
